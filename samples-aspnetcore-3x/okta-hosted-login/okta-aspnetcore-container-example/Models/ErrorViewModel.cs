7f71b52cfb253a83f26debdf27059e122328d3fb
Franco Tiveron <franco.tiveron@gmail.com>
2020-07-24 10:07:28 +1000

    initial commit for okta-aspnetcore-container-example

---
+++ samples-aspnetcore-3x/okta-hosted-login/okta-aspnetcore-container-example/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace okta_aspnetcore_container_example.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
